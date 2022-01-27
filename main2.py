351f814e90ba48e62be05c61d3fac5f3c216fae3
rnadanadia <rnadanadia@gmail.com>
2022-01-27 14:27:43 +0100

    Add project description

--- main2.py
+++ main2.py
@@ -2,14 +2,14 @@ from typing import List
 
 possible_words: List[str] = ['becode', 'learning', 'mathematics', 'session']
 
-def start_game() -> str:
+def start_game():
     lives: int = 5
     wrongly_guessed_letters: List[str] = []
     word_to_find: str = possible_words[0]
     well_guessed_letters: List[str] = len(word_to_find) * [' _ ']
     error_count: int = 1
     turn_count: int = 1
-    while lives > 0 and ' _ ' in well_guessed_letters: -> str :
+    while lives > 0 and ' _ ' in well_guessed_letters:
         """ 
         Function that will perfom the operation based on the number of lives of player and the letter which is guessed.
         While lives is more than 0 OR there is underscore appears in a text: If the length of the letter is 1 and it is 
@@ -17,10 +17,10 @@ def start_game() -> str:
         how many turn the player has played.
         """
         play: str = input("Please write a letter: ")
-        if len(play)!= 1 or not play.isalpha(): -> str:
+        if len(play)!= 1 or not play.isalpha():
             print("The Input Must Contain 1 Alphabet.")
             continue
-        if play in word_to_find: -> str:
+        if play in word_to_find:
             print(f"{play} is in the word")
             print(f"Turn :{turn_count}")
             for i, letter in enumerate(word_to_find):
@@ -46,10 +46,10 @@ def start_game() -> str:
             lives -= 1 
             turn_count += 1
 
-    if lives == 0: -> str: 
+    if lives == 0:
         print("Game Over")
 
-    elif ' _ ' not in well_guessed_letters: -> str:
+    elif ' _ ' not in well_guessed_letters:
         print(f'You found the word: {word_to_find} in {turn_count} turns with {error_count} errors!')
         
         """
